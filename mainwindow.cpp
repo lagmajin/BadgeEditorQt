eda5e83c2c1030de6d53b810d6f5443f635fa9df
Kubo Seiichiro <lagmajin@gmail.com>
2026-05-04 23:39:59 +0900

    Fix ADS CMake package name: qtadvanceddocking -> ads

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -19,8 +19,8 @@
 #include <QDragEnterEvent>
 #include <QDropEvent>
 #include <QPushButton>
-#include <DockManager.h>
-#include <DockWidget.h>
+#include <ads/DockManager.h>
+#include <ads/DockWidget.h>
 #include <QFileInfo>
 #include <cmath>
 
